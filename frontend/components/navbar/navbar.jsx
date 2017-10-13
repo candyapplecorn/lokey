4eab20d1a3e3515647ec17a6befb2a498b3d478a
Joey Bieze <joseph.bieze@gmail.com>
2017-10-13 11:24:14 -0700

    Set Navbar as page footer, style

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -4,10 +4,13 @@ import { Link } from 'react-router-dom';
 const sessionLinks = () => (
   <div className="navbar-div">
     <nav className="logged-out-navbar">
-      <Link className="events-link" to="/events">events</Link>
-      <Link className="create-event-link" to="/sign-in">create event</Link>
-      <Link className="map-link" to="/map">map</Link>
-      <Link className="sign-in-link" to="/sign-up">sign up</Link>
+      <img className="logo" src="http://lokey.gr/wp-content/uploads/2016/05/lokey_logo_300.png"></img>
+      <div className="navbar-links">
+        <Link className="events-link" to="/events">events</Link>
+        <Link className="create-event-link" to="/sign-in">create event</Link>
+        <Link className="map-link" to="/map">map</Link>
+        <Link className="sign-in-link" to="/sign-up">sign up</Link>
+      </div>
     </nav>
   </div>
 );
@@ -15,10 +18,13 @@ const sessionLinks = () => (
 const loggedInSessionLinks = (currentUser, logout) => (
   <div className="navbar-div">
     <nav className="logged-in-navbar">
-      <Link className="events-link" to="/events">events</Link>
-      <Link className="create-event-link" to="/events">create event</Link>
-      <Link className="map-link" to="/map">map</Link>
-      <Link className="profile" to="/profile">profile</Link>
+      <img src="http://lokey.gr/wp-content/uploads/2016/05/lokey_logo_300.png"></img>
+      <div className="navbar-links">
+        <Link className="events-link" to="/events">events</Link>
+        <Link className="create-event-link" to="/events">create event</Link>
+        <Link className="map-link" to="/map">map</Link>
+        <Link className="profile" to="/profile">profile</Link>
+      </div>
     </nav>
   </div>
 );
