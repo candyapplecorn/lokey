df29a7db23558c7f706408b99004f19f43cb1f1e
sunaynab <sunaynabhikha@gmail.com>
2017-10-13 10:12:17 -0700

    Add event actions, reducer and map container

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,10 +1,11 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import {logger} from 'redux-logger';
 import RootReducer from '../reducers/root_reducer';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const configureStore = (preloadedState = {}) => (
-  createStore(RootReducer, preloadedState, applyMiddleware(thunk))
+  createStore(RootReducer, preloadedState, composeEnhancers(applyMiddleware(thunk)))
 );
 
 export default configureStore;
