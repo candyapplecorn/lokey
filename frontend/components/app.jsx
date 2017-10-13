3885646a352952043b754df7885756d83e39ec11
sunaynab <sunaynabhikha@gmail.com>
2017-10-13 09:34:21 -0700

    add map

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -5,6 +5,7 @@ import {
   Switch,
   HashRouter
 } from 'react-router-dom';
+import Map from './map/map';
 
 import SessionFormContainer from './session_form/session_form_container';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
@@ -15,6 +16,7 @@ const App = () => (
     <Switch>
       <AuthRoute path="/sign-in" component={SessionFormContainer} />
       <AuthRoute path="/sign-up" component={SessionFormContainer} />
+      <Route path="/map" component={Map} />
     </Switch>
 </div>
 );
