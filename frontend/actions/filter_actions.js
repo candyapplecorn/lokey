1a6204d9318a92390870438e5ec89a5dca35e962
sunaynab <sunaynabhikha@gmail.com>
2017-10-13 21:56:15 -0700

    add search, map

---
+++ frontend/actions/filter_actions.js
@@ -0,0 +1,14 @@
+import { fetchEvents } from './event_actions';
+
+
+export const UPDATE_BOUNDS = 'UPDATE_BOUNDS';
+
+export const updateBounds = bounds => ({
+  type: UPDATE_BOUNDS,
+  bounds
+});
+
+export const updateFilter = (bounds) => (dispatch, getState) => {
+  dispatch(updateBounds(bounds));
+  return fetchEvents(bounds)(dispatch);
+};
--- frontend/components/map/map.jsx
+++ frontend/components/map/map.jsx
@@ -9,7 +9,6 @@ class ActivityMap extends React.Component {
   }
 
   componentDidMount() {
-    this.props.fetchEvents();
     const mapOptions = {
       center: { lat: 37.7758, lng: -122.435 },
       zoom: 13
@@ -18,12 +17,31 @@ class ActivityMap extends React.Component {
     const map = this.refs.map;
 
     this.map = new google.maps.Map(map, mapOptions);
-    this.MarkerManager = new MarkerManager(this.map)
+    this.MarkerManager = new MarkerManager(this.map);
 
   }
+
+  componentDidUpdate() {
+    // this.MarkerManager.updateMarkers();
+  }
+
+  registerListeners() {
+    google.maps.event.addListener(this.map, 'idle', () => {
+      const { north, south, east, west } = this.map.getBounds().toJSON();
+      const bounds = {
+        northEast: { lat: north, lng: east },
+        southWest: { lat: south, lng: west } };
+      this.props.updateFilter(bounds);
+    });
+    google.maps.event.addListener(this.map, 'click', (event) => {
+      const coords = {lat: event.lat, lng: event.lng};
+      this.handleClick(coords);
+    });
+  }
+
   render() {
     return (
-      <div style={{width: 500, height:500}}
+      <div style={{width: 500, height: 500}}
         id='map'
         ref="map">
       </div>
