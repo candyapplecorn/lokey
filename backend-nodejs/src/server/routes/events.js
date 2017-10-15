e1b67bbd3f8d5479332d0aa0afd251956641d11b
candyapplecorn <candyapplecorn@gmail.com>
2017-10-14 20:50:32 -0700

    added route and controllers for events/index and events/create which correspond to get, post. events/post/create is an authroute. will add event/delete/destroy

---
+++ backend-nodejs/src/server/routes/events.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const EventsController = require('../controllers/events');
+const authHelpers = require('../auth/auth_helpers');
+
+router.get('', (req, res, next)  => {
+  // return authHelpers.createUser(req, res)
+  // .then((response) => {
+  //   passport.authenticate('local', (err, user, info) => {
+  //     if (user) { handleResponse(res, 200, 'success'); }
+  //   })(req, res, next);
+  // })
+  // .catch((err) => { handleResponse(res, 500, 'error'); });
+  return EventsController.INDEX().then((data) => {
+    res.status(200).json(
+      data.reduce((acc, e) => ((acc[e.id] = e), acc), {})
+    );
+  })
+  .catch(err => handleError(res, 500, err))
+});
+
+router.post('', authHelpers.loginRequired, (req, res, next)  => {
+  return EventsController.CREATE(req).then((data) => {
+
+    /* Take the array of events that's returned and turn it
+       into a hash where the events' ids are the hash's keys */
+    res.status(200).json(data)
+  })
+  .catch(err => handleError(res, 500, err))
+});
+
+// function handleResponse(res, code, statusMsg) {
+//   res.status(code).json({status: statusMsg});
+// }
+function handleError(res, code, err) {
+  res.status(code).json({ err });
+}
+
+module.exports = router;
