422c924a6eb47c13700af2cc3f62973ea827c84b
candyapplecorn <candyapplecorn@gmail.com>
2017-10-14 22:06:16 -0700

    events/destroy works now and deletes its dependant coordinate. events/index works. events/post works. NOTE, event/post takes a coordinate key nesting an object with two keys, lat and lng

--- backend-nodejs/src/server/controllers/events.js
+++ backend-nodejs/src/server/controllers/events.js
@@ -1,5 +1,7 @@
 const knex = require('../db/connection');
 
+const err = error => console.log(error);
+
 // Returns a promise, the .then will have the activities array
 function INDEX(){
   return knex.select('*').from('events')
@@ -19,10 +21,34 @@ function CREATE({ user: currentUser, body: formEvent }){
     return knex('events')
     .insert({ host_id, activity_id, description, coordinate_id: Number(coordinate_id) })
     .returning("*")
-  }).catch(err => console.log(err))
+  }).catch(err)
+}
+
+//                 vvvvvv MAKE SURE THAT'S SAME AS UTIL
+function DESTROY({ params: { id: event_id }}){
+  return knex.raw(`
+    DELETE FROM coordinates WHERE coordinates.id = (
+      SELECT coordinate_id
+      FROM events
+      WHERE events.id = ?
+    )`, [event_id])
+  .then((response) => {
+    return knex('events')
+    .where({ id: event_id })
+    .del();
+    })
+  ;
+
+  /*knex.select("*")
+  .from('events')
+  .where({ id: event_id })
+  .then(event => { // coordinate_id, host_id, activity_id
+    console.log(event)
+  }).catch(err)*/
 }
 
 module.exports = {
   INDEX,
-  CREATE
+  CREATE,
+  DESTROY
 };
