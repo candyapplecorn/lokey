41bba54ca6c7dcedfce2eee7203e013e5e6808a8
candyapplecorn <candyapplecorn@gmail.com>
2017-10-14 22:04:41 -0700

    finally got coordinates to delete when deleting an event. did this with RAW, will probably use raw more now as it is just easier.

--- backend-nodejs/src/server/db/migrations/20171014201231_create_events.js
+++ backend-nodejs/src/server/db/migrations/20171014201231_create_events.js
@@ -1,11 +1,16 @@
 
 exports.up = (knex, Promise) =>
   knex.schema.createTable('events', t => {
+    t.integer('coordinate_id').unsigned();
+    t.integer('host_id').unsigned();
+    t.integer('activity_id').unsigned();
+
     t.increments()
-    t.integer('coordinate_id').unsigned().references('id').inTable('coordinates');
-    t.integer('host_id').unsigned().references('id').inTable('users');
-    t.integer('activity_id').unsigned().references('id').inTable('activities');
     t.text('description').defaultTo("")
+
+    t.foreign('coordinate_id').references('id').inTable('coordinates').onDelete('CASCADE');
+    t.foreign('host_id').references('id').inTable('users');
+    t.foreign('activity_id').references('id').inTable('activities');
   })
 ;
 
