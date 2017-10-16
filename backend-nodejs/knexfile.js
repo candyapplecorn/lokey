017d0358636b69ee81f503b6cb8b6fa3e90db6c7
candyapplecorn <candyapplecorn@gmail.com>
2017-10-15 22:21:51 -0700

    fixed a bug that was not documented in stack overflow, blog posts, or the official knex documentation. i had to read through error comments in issue threads on the knex page from 2014 and 2015. basically, the problem was getting knex to recognize a pool with which to access the postgresql database on heroku. eventually i got it working thanks to the documentation on the npm package page pg-pool, as well as the error threads from knexs github repo. other changes.... just making this thing more friendly to heroku, yeah.

--- backend-nodejs/knexfile.js
+++ backend-nodejs/knexfile.js
@@ -1,4 +1,12 @@
 const databaseName = 'passport_local_knex';
+const Pool = require('pg-pool');
+const url = require('url');
+
+// *** load environment variables *** //
+require('dotenv').config();
+
+var pg = require('pg');
+pg.defaults.ssl = true;
 
 module.exports = {
   development: {
@@ -10,6 +18,32 @@ module.exports = {
     seeds: {
       directory: __dirname + '/src/server/db/seeds'
     }
+  },
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      //directory: __dirname + 'backend-nodejs/src/server/db/migrations'
+      directory: __dirname + '/src/server/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/src/server/db/seeds'
+    },
+    // pool: (() => {
+    //   console.log(process.env)
+    //   var params = url.parse(process.env.DATABASE_URL);
+    //   var auth = params.auth.split(':');
+    //   var pconfig = {
+    //     user: auth[0],
+    //     password: auth[1],
+    //     host: params.hostname,
+    //     port: params.port,
+    //     database: params.pathname.split('/')[1],
+    //     ssl: true
+    //   };
+    //
+    //   var pool = new Pool(pconfig);
+    // })()
   },
   test: {
     client: 'postgresql',
