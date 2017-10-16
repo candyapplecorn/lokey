d51f642a6b12a50aa73d6800cbecceac0aa8341a
candyapplecorn <candyapplecorn@gmail.com>
2017-10-15 19:04:59 -0700

    made the activities index send the same data that the rails server sends: an array of names, rather than of objects with activity_id and name

--- backend-nodejs/src/server/controllers/activities.js
+++ backend-nodejs/src/server/controllers/activities.js
@@ -3,6 +3,11 @@ const knex = require('../db/connection');
 // Returns a promise, the .then will have the activities array
 function INDEX(){
   return knex('activities').select('id', 'name')
+  .map(row => row.name)
+  // .then(as =>
+  //   as.map(ao => a.name)
+  // )
+  //.catch(err => console.log(err))
 }
 
 module.exports = {
