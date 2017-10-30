030b7eae395a0ed635ed94044e482e6283836c27
Joseph Burger <candyapplecorn@gmail.com>
2017-10-30 09:01:50 -0700

    added secrets.yml setup including generating a secret key bash variable, and copying over a secrets.yml for development

--- install_mac_mini.sh
+++ install_mac_mini.sh
@@ -1,6 +1,11 @@
 bundle && \
 npm i && \
 bundle exec figaro install && \
+curl https://raw.githubusercontent.com/candyapplecorn/write-it-up/master/Rakefile > Rakefile && \
+secret=`rails secret` && \
+cp config/development_secrets.yml config/secrets.yml && \
 cd backend-nodejs && \
 npm i && \
-npm i -g knex
+npm i -g knex && \
+cd .. && \
+rails db:setup
