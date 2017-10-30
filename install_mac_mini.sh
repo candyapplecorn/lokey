1ec1008dbfe0a7929aa3c80b02af7f3352d66468
Joseph Burger <candyapplecorn@gmail.com>
2017-10-30 10:52:58 -0700

    added git configs to install

--- install_mac_mini.sh
+++ install_mac_mini.sh
@@ -1,3 +1,7 @@
+# CHANGE THESE IF YOU'RE NOT JOE
+git config user.email "candyapplecorn@gmail.com"
+git config user.name "Joseph Burger"
+
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash;
 nvm install 8.8.1;
 
